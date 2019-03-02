e936a7ea9b981ce755f45adce66605a344d19785
Brian Spaulding <bspaulding@live.com>
2019-03-02 12:06:21 -0800

    Cleaning up user-agent

--- turing_bot.py
+++ turing_bot.py
@@ -477,7 +477,8 @@ def stockcheck():
         return
     try:
         url = 'https://finance.yahoo.com/quote/%s' % stock
-        req  = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+        req  = requests.get(url, headers=headers)
         content = req.text.encode("utf-8") 
         name = re.search('Summary\sfor\s(.*?)\s-\sYahoo\sFinance', content).group(1)
         ticker = stock.upper()
